b2141f6449e5e0a9a134880273b3d899ec3a8e10
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-10-25 22:19:26 +0530

    fix: dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import './app.style.css';
 import DataTable from './components/DataTable';
+import { dummyData } from './data/dummyData';
 
 const App = () => {
 	const [apiData, setApiData] = useState();
 	const fetchApiData = async () => {
-		const response = await fetch(
-			'https://62a6bb9697b6156bff7e6251.mockapi.io/v1/apis'
-		);
-		if (response.status === 200) {
-			const responseData = await response.json();
-			if (responseData?.length) {
-				setApiData(responseData);
-			}
-		}
+		// const response = await fetch(
+		// 	'https://62a6bb9697b6156bff7e6251.mockapi.io/v1/apis'
+		// );
+		// if (response.status === 200) {
+		// 	const responseData = await response.json();
+		// 	if (responseData?.length) {
+		// 		setApiData(responseData);
+		// 	}
+		// }
+
+		//for testing
+		setApiData(dummyData);
 	};
 	useEffect(() => {
 		fetchApiData();
--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { ReactComponent as DeleteSvg } from '../icons/delete.svg';
 import { ReactComponent as EditSvg } from '../icons/edit.svg';
 import { ReactComponent as MaximizeSvg } from '../icons/maximize.svg';
@@ -6,6 +6,7 @@ import { ReactComponent as MinimizeSvg } from '../icons/minimize.svg';
 
 const DataTable = ({ apiData = [] }) => {
 	console.log('apiData ==>> ', apiData);
+	const [selectedData, setSelectedData] = useState();
 	const formatDateStr = (str) => {
 		return str.split('T')[0].split('-').reverse().join('/');
 	};
@@ -25,29 +26,44 @@ const DataTable = ({ apiData = [] }) => {
 					</tr>
 				</thead>
 				<tbody>
+					{/* for testing */}
 					{apiData.slice(0, 6).map((data) => (
-						<tr key={data.id}>
-							<td data-column="actions">
-								<div>
-									<button>
-										<DeleteSvg />
-									</button>
-									<button>
-										<EditSvg />
-									</button>
-									<button>
-										<MaximizeSvg />
-									</button>
-								</div>
-								<input type="radio" checked="checked"></input>
-							</td>
-							<td data-column="id">{data.id}</td>
-							<td data-column="name">{data.name}</td>
-							<td data-column="type">{data.type}</td>
-							<td data-column="description">{data.description}</td>
-							<td data-column="createdAt">{formatDateStr(data.createdAt)}</td>
-							<td data-column="updatedAt">{formatDateStr(data.updatedAt)}</td>
-						</tr>
+						<Fragment key={data.id}>
+							<tr key={data.id}>
+								<td data-column="actions">
+									<div>
+										<button>
+											<DeleteSvg />
+										</button>
+										<button>
+											<EditSvg />
+										</button>
+										<button>
+											<MaximizeSvg />
+										</button>
+									</div>
+									<input
+										type="radio"
+										readOnly
+										checked={selectedData?.id === data.id}
+									></input>
+								</td>
+								<td data-column="id">{data.id}</td>
+								<td data-column="name">{data.name}</td>
+								<td data-column="type">{data.type}</td>
+								<td data-column="description">{data.description}</td>
+								<td data-column="createdAt">{formatDateStr(data.createdAt)}</td>
+								<td data-column="updatedAt">{formatDateStr(data.updatedAt)}</td>
+							</tr>
+							<div style={{ display: 'none' }}>
+								<br />
+								hidden row
+								<br />
+								hidden row
+								<br />
+								hidden row
+							</div>
+						</Fragment>
 					))}
 				</tbody>
 			</table>
